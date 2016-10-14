c3d27daf7915e871a8f9cb52994111422ac16cbe
Ben Scheiner <ben@carevoyance.com>
2016-10-14 15:03:10 -0700

    fix sound and functionalize control.js

--- src/control.js
+++ src/control.js
@@ -1,19 +1,16 @@
 
+function loadSong() {
+  var song = new buzz.sound("assets/sound/suonatore_di_liuto", {
+    formats: ['mp3'],
+    preload: true
+  });
+  song.bind('canplaythrough', function() {
+    console.log('here')
+    this.play();
+  });
+}
 
-
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJtc2NoZWluZXIiLCJhIjoiMEJvZTZsTSJ9.iB00l7bUVL9WUxx7YwhOdA', {
-  attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-});
-var map = L.map('map')
-.addLayer(mapboxTiles)
-.setView([6.550786211171341, 41.01753173828125], 9)
-.on('click', function(e) {
-  console.log(map.getZoom());
-  console.log(e.latlng.lng + ', ' + e.latlng.lat);
-});
-
-var promise = $.getJSON("items.json");
-promise.then(function(data) {
+function addMapFeatures(data) {
   var items = L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       let icon_path = 'assets/icons/' + feature.properties.icon;
@@ -44,7 +41,23 @@ promise.then(function(data) {
     }
   });
   items.addTo(map);
+}
+
+var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJtc2NoZWluZXIiLCJhIjoiMEJvZTZsTSJ9.iB00l7bUVL9WUxx7YwhOdA', {
+  attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
 });
-//
-// var song = new buzz.sound("assets/sound/suonatore_di_liuto.mp3", {
-// });
+var map = L.map('map')
+.addLayer(mapboxTiles)
+.setView([6.550786211171341, 41.01753173828125], 9)
+.on('click', function(e) {
+  console.log(map.getZoom());
+  console.log(e.latlng.lng + ', ' + e.latlng.lat);
+});
+
+var promise = $.getJSON("items.json");
+promise.then(function(data) {
+  addMapFeatures(data);
+  loadSong();
+});
+
+
